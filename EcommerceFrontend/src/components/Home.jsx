eba927039938149136510b1c16f50f3507814e80
raziqpv333 <raziqpv333@gmail.com>
2024-07-31 09:22:31 +0530

    view

--- EcommerceFrontend/src/components/Home.jsx
+++ EcommerceFrontend/src/components/Home.jsx
@@ -1,5 +1,30 @@
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+
 const Home = () => {
-    return <h1>Home</h1>;
+  const [product,setProduct]=useState([]);
+  useEffect(()=>{
+    axios.get('https://apiecommerce.pythonanywhere.com/api/product/')
+    .then (response=> setProduct(response.data))
+    .catch(error=>console.log(error));
+  },[])
+    return(
+      <div className="container mt-3">
+        {/* console.log(product); */}
+        <h2>dfhgfsghfhsg</h2>
+        {product.map(product=>(
+          
+           <div className="card">
+           <div className="card-body">  
+            
+             <h3 className="card-title">{product.p_name}</h3>
+             <p className="card-text">{product.price}</p>
+           </div>
+           </div>     
+        ))}
+       
+      </div>
+    )
   };
   
   export default Home;
